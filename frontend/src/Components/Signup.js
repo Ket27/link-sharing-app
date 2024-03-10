e9a30a2a6c52a87800c2cd333d0e9b50f437c640
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2024-03-10 19:55:58 +0530

    Update Signup.js

--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -31,7 +31,7 @@ const Signup = () => {
       };
 
       const { data } = await axios.post(
-        "http://localhost:8080/api/auth/signup",
+        "http://link-sharing-app-sigma.vercel.app/api/auth/signup",
         { name, email, password },
         config
       );

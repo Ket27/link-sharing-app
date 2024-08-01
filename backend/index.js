edb12858680d5ca540795c79b0a833230b1cc29a
Ketan Mattoo <112501232+Ket27@users.noreply.github.com>
2024-08-01 15:14:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ connectdB();
 const Port = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'link-sharing-app-1u99.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-type');
     next();

322efe8779930e43211619a640a603e5fd2b7c62
Ketan <ketanmattoo27@gmail.com>
2024-12-13 00:37:20 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,15 @@ const dotenv = require('dotenv');
 const connectdB = require('./config/db');
 const userRouter = require('./routers/userRouter');
 const linkRouter = require("./routers/linkRouter");
+const path = require("path");
 
 app.use(express.json());
 app.use(bodyParser.json());
 dotenv.config();
 connectdB();
 const Port = process.env.PORT || 8080;
+
+const _dirname = path.resolve();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -23,6 +26,11 @@ app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', userRouter);
 app.use('/api/links', linkRouter);
+
+app.use(express.static(path.join(_dirname,"/frontend/build")));
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(_dirname,"frontend", "build", "index.html"));
+})
 
 app.listen(8080, () => {
     console.log(`Server is running on port ${Port}`);
